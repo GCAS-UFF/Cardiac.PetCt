d8b9a106b7c5446c0f66338ca978a28e366851c0
Giovane Lopes <giovanelopes>
2020-11-25 19:36:19 -0300

    adjusts theme brightness

--- lib/core/ui/date_picker_app.dart
+++ lib/core/ui/date_picker_app.dart
@@ -15,95 +15,88 @@ class DatePickerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
-    return Theme(
-      data: ThemeData.light().copyWith(
-        //Date picker color
-        primaryColor: ColorsApp.greenApp,
-      ),
-      child: new Builder(
-        builder: (context) => InkWell(
-          onTap: () {
-            // Show date picker action
-            showDatePicker(
-              context: context,
-              
-              initialDate: DateTime.now(),
-              firstDate: DateTime(DateTime.now().year),
-              lastDate: DateTime(DateTime.now().year + 5),
-            ).then((date) {
-              onChange(date);
-            });
-          },
-          child: Stack(
-            children: <Widget>[
-              Container(
-                width: Dimensions.getConvertedWidthSize(context, 165),
-                padding: Dimensions.getEdgeInsets(context, right: 10, left: 15),
-                margin: Dimensions.getEdgeInsets(
-                  context,
-                  top: 20,
-                  bottom: 20,
+    return Builder(
+      builder: (context) => InkWell(
+        onTap: () {
+          // Show date picker action
+          showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(DateTime.now().year),
+            lastDate: DateTime(DateTime.now().year + 5),
+          ).then((date) {
+            onChange(date);
+          });
+        },
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: Dimensions.getConvertedWidthSize(context, 165),
+              padding: Dimensions.getEdgeInsets(context, right: 10, left: 15),
+              margin: Dimensions.getEdgeInsets(
+                context,
+                top: 20,
+                bottom: 20,
+              ),
+              height: Dimensions.getConvertedHeightSize(context, 50),
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(
+                  Dimensions.getConvertedWidthSize(context, 5),
                 ),
-                height: Dimensions.getConvertedHeightSize(context, 50),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(
-                    Dimensions.getConvertedWidthSize(context, 5),
-                  ),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        //Value
-                        value == null
-                            ? ""
-                            : DateHelper.convertDateToString(
-                                value,
-                              ),
-                        style: GoogleFonts.montserrat(
-                          fontSize: Dimensions.getTextSize(
-                            context,
-                            14,
-                          ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      //Value
+                      value == null
+                          ? ""
+                          : DateHelper.convertDateToString(
+                              value,
+                            ),
+                      style: GoogleFonts.montserrat(
+                        fontSize: Dimensions.getTextSize(
+                          context,
+                          14,
                         ),
                       ),
                     ),
-                    // Dropdown icon
-                    Icon(Icons.arrow_drop_down,
-                        size: Dimensions.getConvertedWidthSize(context, 30),
-                        color: (_themeChanger.getThemeData() == false)
-                            ? Colors.black
-                            : Colors.white),
+                  ),
+                  // Dropdown icon
+                  Icon(Icons.arrow_drop_down,
+                      size: Dimensions.getConvertedWidthSize(context, 30),
+                      color: (_themeChanger.getThemeData() == false)
+                          ? Colors.black
+                          : Colors.white),
+                ],
+              ),
+            ),
+            //Label
+            Positioned(
+              top: Dimensions.getConvertedHeightSize(context, 11),
+              left: Dimensions.getConvertedHeightSize(context, 10),
+              child: Container(
+                padding: Dimensions.getEdgeInsetsAll(context, 3),
+                color: (_themeChanger.getThemeData() == false)
+                    ? ColorsApp.lightBackground
+                    : ColorsApp.darkBackground,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Text(
+                      Strings(context).dateLabel,
+                      style: GoogleFonts.montserrat(
+                        fontSize: Dimensions.getTextSize(context, 15),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ],
                 ),
               ),
-              //Label
-              Positioned(
-                top: Dimensions.getConvertedHeightSize(context, 11),
-                left: Dimensions.getConvertedHeightSize(context, 10),
-                child: Container(
-                  padding: Dimensions.getEdgeInsetsAll(context, 3),
-                  color: (_themeChanger.getThemeData() == false)
-                      ? ColorsApp.lightBackground
-                      : ColorsApp.darkBackground,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Text(
-                        Strings(context).dateLabel,
-                        style: GoogleFonts.montserrat(
-                          fontSize: Dimensions.getTextSize(context, 15),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/core/utils/theme.dart
+++ lib/core/utils/theme.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ThemeChanger with ChangeNotifier {
-  ThemeData _themeData = ThemeData.light();
+  ThemeData _themeData;
+  ThemeData _themeDataLight =
+      ThemeData(brightness: Brightness.light, primarySwatch: color);
+  ThemeData _themeDataDark =
+      ThemeData(brightness: Brightness.dark, primarySwatch: color);
 
   ThemeChanger(this._themeData);
 
   getTheme() => _themeData;
 
   getThemeData() {
-    if (_themeData == ThemeData.dark()) {
+    if (_themeData.brightness == Brightness.dark) {
       return true;
     } else {
       return false;
@@ -16,11 +20,27 @@ class ThemeChanger with ChangeNotifier {
   }
 
   changeTheme() {
-    if (_themeData == ThemeData.light()) {
-      _themeData = ThemeData.dark();
+    if (_themeData.brightness == Brightness.light) {
+      _themeData = _themeDataDark;
     } else {
-      _themeData = ThemeData.light();
+      _themeData = _themeDataLight;
     }
     notifyListeners();
   }
 }
+
+MaterialColor color = MaterialColor(
+  0xFF06BEA6,
+  <int, Color>{
+    50: Color(0xFF06BEA6),
+    100: Color(0xFF06BEA6),
+    200: Color(0xFF06BEA6),
+    300: Color(0xFF06BEA6),
+    400: Color(0xFF06BEA6),
+    500: Color(0xFF06BEA6),
+    600: Color(0xFF06BEA6),
+    700: Color(0xFF06BEA6),
+    800: Color(0xFF06BEA6),
+    900: Color(0xFF06BEA6),
+  },
+);
